bc09c2942060b4cab752a503f641f0158d0738bb
varanshu-garg <varanshugarg@gmail.com>
2021-08-02 22:33:59 +0530

    added api link

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -26,7 +26,7 @@ export default function Dashboard({code}) {
   useEffect(() =>{
     if(!playingTrack) return
     axios
-    .get("http://localhost:3001/lyrics",{
+    .get("https://clone-of-spotify.herokuapp.com/lyrics",{
       params: {
         track: playingTrack.title,
         artist: playingTrack.artist,
